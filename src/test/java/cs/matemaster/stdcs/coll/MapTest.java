ad2bc6a6586c3e2ffc43e4400700ccdd9fd2f5ad
matemaster <matemaster@163.com>
2022-10-07 15:34:49 +0800

    #test map-api测试

--- src/test/java/cs/matemaster/stdcs/coll/MapTest.java
+++ src/test/java/cs/matemaster/stdcs/coll/MapTest.java
@@ -17,7 +17,8 @@ public class MapTest {
         Map<String, Object> map = new HashMap<>();
         map.put("user", "mate-master");
         map.put("passwd", "JCH980JS1Dcv2j9");
-        map.compute("user", (k, v) -> StringUtils.isNotEmpty(k) ? "name: " + v : "name:null");
+        Object compute = map.compute("user", (k, v) -> StringUtils.isNotEmpty(k) ? "name: " + v : "name:null");
+        System.out.println(compute);
         System.out.println(map);
     }
 
@@ -32,5 +33,112 @@ public class MapTest {
 
         ImmutableMap<String, String> immutableMap = ImmutableMap.of("user", "mate-master");
         System.out.println(immutableMap);
+    }
+
+    @Test
+    public void contains() {
+        Map<String, Object> map = new HashMap<String, Object>() {{
+            put("user", "mate-master");
+            put("passwd", "JCH980JS1Dcv2j9");
+        }};
+
+        boolean containsKey = map.containsKey("user");
+        boolean containsValue = map.containsValue("mate-master");
+        System.out.println(containsKey);
+        System.out.println(containsValue);
+    }
+
+    @Test
+    public void replace() {
+        Map<String, Object> map = new HashMap<String, Object>() {{
+            put("user", "mate-master");
+            put("passwd", "JCH980JS1Dcv2j9");
+        }};
+
+        Object replace = map.replace("user", 100);
+        System.out.println(replace);
+
+        boolean result = map.replace("user", 100, "Matemaster");
+        System.out.println(result);
+
+        System.out.println(map);
+    }
+
+    @Test
+    public void replace_1() {
+        Map<String, Object> map = new HashMap<String, Object>() {{
+            put("user", "mate-master");
+            put("passwd", "JCH980JS1Dcv2j9");
+        }};
+
+        Object replace = map.replace("username", 100);
+        System.out.println(replace);
+
+        boolean result = map.replace("user", 200, "Matemaster");
+        System.out.println(result);
+
+        System.out.println(map);
+    }
+
+    @Test
+    public void computeIfAbsent() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user", "macro");
+        map.computeIfAbsent("user", (v) -> v.equals("macro") ? "matemaster" : v);
+        map.computeIfPresent("user", (k, v) -> v.equals("macro") ? "matemaster" : v);
+        System.out.println(map);
+    }
+
+    @Test
+    public void getOrDefault() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user", "matemaster");
+        Object user = map.get("user");
+        Object orDefault = map.getOrDefault("username", "username");
+        System.out.println(user);
+        System.out.println(orDefault);
+    }
+
+    @Test
+    public void replaceAll() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user", "matemaster");
+        map.put("passwd", "matemaster");
+
+        map.replaceAll((key, value) -> {
+            if (key.length() <= 4) {
+                return null;
+            } else {
+                return value;
+            }
+        });
+
+        System.out.println(map);
+    }
+
+    @Test
+    public void set() {
+        Map<String, String> map = new HashMap<String, String>() {{
+            put("port", "8080");
+            put("server", "localhost");
+            put("driver", "mysql");
+            put("framework", "spring");
+        }};
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry);
+        }
+
+        for (String key : map.keySet()) {
+            System.out.println(key);
+        }
+
+        for (String value : map.values()) {
+            System.out.println(value);
+        }
+
+        map.forEach((key, value) -> {
+            System.out.println(key + ": " + value);
+        });
     }
 }
