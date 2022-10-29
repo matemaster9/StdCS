8584073d34130063fe0ee647c302665b32b91af3
matemaster <matemaster@163.com>
2022-10-29 13:42:41 +0800

    #chore 算法4模块

---
+++ ibee-algs4/src/main/java/cs/matemaster/algs4/Main.java
@@ -0,0 +1,10 @@
+package cs.matemaster.algs4;
+
+/**
+ * @author ${USER}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
